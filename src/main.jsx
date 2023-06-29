fdedc76362823ac01d3050c93fe936072763dcdc
Brian Kinyua <134489011+Brikita@users.noreply.github.com>
2023-06-29 16:08:32 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Confetti from "react-confetti"
+
+      
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <div >
+      <Confetti />
+      <App />
+  </div>
+)
