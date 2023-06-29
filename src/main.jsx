e64952c2a577095f7758b1963942cda7a6b0de3b
Brian Kinyua <134489011+Brikita@users.noreply.github.com>
2023-06-29 16:23:05 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Confetti from "react-confetti"
+import confetti from 'https://cdn.skypack.dev/canvas-confetti'
 
-      
+function makeConfetti(){
+      confetti()
+}
+
+const btn = document.getElemetById("btn")
+
+btn.addEventListener("click", makeConfetti)
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <div >
-      <Confetti />
-      <App />
-  </div>
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
