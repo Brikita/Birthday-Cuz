5ebb1f1bf619469a9ecb8f2b454153685a687751
Brian <134489011+Brikita@users.noreply.github.com>
2023-06-29 15:16:47 +0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Confetti from "react-confetti"
+import  ReactDOM  from "react-dom/client";
+
+
+function App() {
+  
+
+  return (
+      <div >
+        <Confetti />
+        <h1>Happy Birthday Cuz!</h1>
+        <p>Dear Alice,</p>
+        <p>On your special day, I want to take a moment to wish you the happiest birthday ever!</p>
+        <p>You are incredibly special to me, and I'm grateful to have you in my life. Your presence brings joy and warmth to everyone around you. You deserve all the best that life has to offer.</p>
+        <p>May this birthday be filled with love, laughter, and beautiful moments that will create unforgettable memories. Enjoy your day to the fullest!</p>
+        <p>With all my love,</p>
+        <p>Brian Kinyua</p>
+
+      </div>
+  )
+}
+
+export default App
